600b8ac89ae6a7a368565cfa1195720406e7dd30
Michael Mior <mmior@uwaterloo.ca>
2016-03-11 09:14:15 -0500

    Try Mac OS build

---
+++ script/homebrew-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew update
+
+brew install bison
+brew install cabal-install
+brew install flex
+brew install git
+brew install openssl
+brew install tmux
+brew install vim
+brew install zsh
